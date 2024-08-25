c7c200516056c90fab39ac913a03b21a2cd77629
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-25 17:25:38 +0530

    sqaure pattern

--- basic/patterns.cpp
+++ basic/patterns.cpp
@@ -3,7 +3,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    cout<<n;
-    cout<<"hello world";
+    //square pattern
+    for(int i=0;i<n;i++){
+        cout<<"*";
+        for(int j=1;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    //traingle pattern
     
 }
