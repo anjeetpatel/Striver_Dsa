3e159d048a88c84b23fe052e1f0fcea7359c5552
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-25 17:41:05 +0530

    Patterns

--- basic/patterns.cpp
+++ basic/patterns.cpp
@@ -1,18 +1,52 @@
 #include<iostream>
 using namespace std;
+
+void SqaurePattern(int n){
+    // square pattern
+    for (int i = 0; i < n; i++)
+    {
+        cout << "*";
+        for (int j = 1; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void TrianglePattern(int n){
+    // traingle pattern
+    for (int i = 0; i < n; i++)
+    {
+        cout << "*";
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void NumericTriangle(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 int main(){
     int n;
     cin>>n;
-    //square pattern
-    for(int i=0;i<n;i++){
-        cout<<"*";
-        for(int j=1;j<n;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    cout<<endl;
-
-    //traingle pattern
+    SqaurePattern(n);
     
+    TrianglePattern(n);
+
+    NumericTriangle(n);
 }
